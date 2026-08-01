f4d92ce2145718030d4472cdd72ee75226451042
Sakhawat Ali <msakhawatali131@gmail.com>
2026-08-01 15:21:39 +0500

    feat: add health check endpoint

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -2,6 +2,11 @@ from fastapi import APIRouter
 
 api_router = APIRouter()
 
+@api_router.get("/v1/health")
+def health():
+    return {"health" : "My Health is Good"}
+
+
 # Future feature routers will be included here:
 # api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 # api_router.include_router(products.router, prefix="/products", tags=["products"])
