c43f9fdf7d77b55cc0b4aa67090a14a08a5433d8
Sakhawat Ali <msakhawatali131@gmail.com>
2026-08-05 15:48:48 +0500

    feat: create Product SQLAlchemy model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.base import Base
 from app.models.user import User
+from app.models.product import Product
 
-__all__ = ["Base", "User"]
+__all__ = ["Base", "User", "Product"]
---
+++ backend/app/models/product.py
@@ -0,0 +1,19 @@
+from app.db.base import Base
+from sqlalchemy import Column, Integer, String, Boolean, Numeric, func, DateTime
+
+
+class Product():
+    __tablename__ = "product"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=True)
+    slug = Column(String, unique=True, nullable=False, index=True)
+    description = Column(String, nullable=True)
+    sku = Column(String, unique=True, nullable=False, index=True)
+    category = Column(String, nullable=False)
+    brand = Column(String, nullable=True)
+    price = Column(Numeric(10, 2), nullable=False)
+    stock_quantity = Column(Integer, default=0, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    create_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
