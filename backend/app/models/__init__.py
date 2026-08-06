ff3c52e77fc5b669927d9ed54d85bb52db441156
Sakhawat Ali <msakhawatali131@gmail.com>
2026-08-06 17:06:20 +0500

    Create Brand SQLAlchemy model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,6 @@ from app.models.base import Base
 from app.models.user import User
 from app.models.product import Product
 from app.models.product_category import ProductCategory
+from app.models.brand import Brand
 
-__all__ = ["Base", "User", "Product", "ProductCategory"]
+__all__ = ["Base", "User", "Product", "ProductCategory", "Brand"]
