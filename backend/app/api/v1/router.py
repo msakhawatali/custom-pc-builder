b6a39af5be6d6fd5c92663ea2d3b08713e4b5b68
Sakhawat Ali <msakhawatali131@gmail.com>
2026-09-17 15:23:59 +0500

    feat: add supplier CRUD API

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints.product_category import router as product_category_router
 from app.api.v1.endpoints.brand import router as brand_router
+from app.api.v1.endpoints.supplier import router as supplier_router
+
 
 
 api_router = APIRouter()
@@ -11,3 +13,4 @@ def health():
 
 api_router.include_router(product_category_router)
 api_router.include_router(brand_router)
+api_router.include_router(supplier_router)
